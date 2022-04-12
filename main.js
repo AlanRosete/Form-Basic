d645f95dad3bd38806d15a793981343930d23df6
Alan Rosete <alanroset3@gmail.com>
2022-04-12 00:26:46 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function captura() {
+    let email = document.getElementById("1").value;
+    let password = document.getElementById("2").value;
+    console.log(email + "" + password)
+}
+
+console.log("hola");
