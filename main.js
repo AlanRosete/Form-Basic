70a97443454627e209a499fdbcb30abe6f45fd0e
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2023-08-03 13:29:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
 
-function captura() {
-    let email = document.getElementById("1").value;
-    let password = document.getElementById("2").value;
-    console.log(email + "" + password)
-}
-
-console.log("hola");
